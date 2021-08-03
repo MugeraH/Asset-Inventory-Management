f48bdd0831aaa9a07ec33dc614d6592872607bcd
jn-mic <jackotienokey@gmail.com>
2021-08-04 00:24:45 +0300

    creating save profile @receiver

--- user/signals.py
+++ user/signals.py
@@ -9,3 +9,8 @@ def create_profile(sender,instance,created, *args,**kwargs):
         Profile.objects.create(employee=instance)
 
 
+@receiver(post_save,sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
