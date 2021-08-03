df04121ee6d9f44d804a06e94d3a58732ad903e9
jn-mic <jackotienokey@gmail.com>
2021-08-04 00:37:23 +0300

    generating a user registration func on a user views app'

--- user/views.py
+++ user/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import login_required
+from .forms import CreateUserForm, UserUpdateForm, ProfileUpdateForm
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            group = Group.objects.get(name='Employees')
+            user.groups.add(group)
+            return redirect('user-login')
+    else:
+        form = CreateUserForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'user/register.html', context)
