06ad1e3688a86b0df8257c5d060764e0da681f77
jn-mic <jackotienokey@gmail.com>
2021-08-04 01:11:13 +0300

    creating a user profile view and its corresponding templates

--- user/views.py
+++ user/views.py
@@ -19,3 +19,9 @@ def register(request):
         'form': form
     }
     return render(request, 'user/register.html', context)
+
+def profile(request):
+    context = {
+        
+    }
+    return render(request, 'user/profile.html', context)
