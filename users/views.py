98317a44800b492f7831ec1072fbebf27abd51a1
jn-mic <jackotienokey@gmail.com>
2021-08-04 12:23:40 +0300

    importing generic CreateView

--- users/views.py
+++ users/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import redirect
+from django.views.generic import CreateView
 
 # Create your views here.
