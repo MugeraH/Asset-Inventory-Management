0d0882b6c9318c0e15daa1e3c723fa99ce28025a
jn-mic <jackotienokey@gmail.com>
2021-08-04 14:08:10 +0300

    creating admin view

--- users/views.py
+++ users/views.py
@@ -32,3 +32,17 @@ class ManagerSignUpView(CreateView):
         user = form.save()
         login(self.request, user)
         return redirect('manager:manager')
+
+class AdminSignUpView(CreateView):
+    model = User
+    form_class = AdminSignUpForm
+    template_name = 'registration/signup_form.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['user_type'] = 'admin'
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('admin:admin')
