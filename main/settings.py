803299b882e9ace84e83a4838a15cbdfa368016f
ephraim-kamau54 <ephraim.kamau@student.moringaschool.com>
2021-08-06 11:23:59 +0300

    all changes

--- main/settings.py
+++ main/settings.py
@@ -29,7 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 INSTALLED_APPS = [
       'assets',
       'users',
-   
+      'adminManagerEmployee',
+
      
     
      # installed dependancies
