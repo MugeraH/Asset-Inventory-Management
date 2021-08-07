d09813437600bf50aa8e93432ddcf9dc796d792b
mugerah <hughes.mugera@touchinspiration.com>
2021-08-07 13:52:00 +0300

    edit models allow null for empoyee and manager request user field

--- assets/urls.py
+++ assets/urls.py
@@ -12,7 +12,8 @@ app_name ="assets"
 urlpatterns=[
     
 path('',HomePageView,name='home'),
-
+path('employees',views.EmployeesView,name='employees'),
+path('dashboard',DashBoardView,name='dashboard'),
 path('forms/',views.departments,name='forms'),
 path('update/department/<int:id>/',views.update_department,name='update_department'),
 path('departments/',views.departments,name='departments'),
@@ -20,7 +21,7 @@ path('asset/',views.asset,name='assetform'),
 path('department/',views.add_departments,name='departmentform'),
 path('employeeassetrequest/',views.employeeassetrequest,name='employeeassetrequest'),
 path('managerrequest/',views.managerrequest,name='managerrequest'),
-path('dashboard',DashBoardView,name='dashboard'),
+
 
 
 
