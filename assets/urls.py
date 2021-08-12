6183fc60cbe9619d87f844afe1a94e58b9b21324
Rubyroy12 <ibrakipz7@gmail.com>
2021-08-12 14:48:33 +0300

    fix bugs in request page

--- assets/urls.py
+++ assets/urls.py
@@ -29,58 +29,58 @@ urlpatterns=[
 
 
    
-path('dept_employees',views.dept_employees,name='dept_employees'),
-path('employee_assets',views.employee_assets,name='employee_assets'),
-
-   
-path('dashboard',DashBoardView,name='dashboard'),
-path('manager_dashboard',views.managerDashBoardView,name='manager_dashboard'),
-path('employee_dashboard',views.employeeDashBoardView,name='employee_dashboard'),
-
-path('employeedetails/<int:id>', views.employeedetails,name='employeedetails'),
+    path('dept_employees',views.dept_employees,name='dept_employees'),
+    path('employee_assets',views.employee_assets,name='employee_assets'),
 
 
+    path('dashboard',DashBoardView,name='dashboard'),
+    path('manager_dashboard',views.managerDashBoardView,name='manager_dashboard'),
+    path('employee_dashboard',views.employeeDashBoardView,name='employee_dashboard'),
 
-path('department_detail/<int:id>',views.department_detail,name='department_detail'),
-path('assets/',views.assets,name='assets'),
-path('dept_assets/',views.dept_assets,name='dept_assets'),
-
-path('update/asset/<int:id>/',views.update_asset,name='assetform'),
-path('employeeassetrequest/',views.employeeassetrequest,name='employeeassetrequest'),
-
-path('assign_asset/<int:id>',views.assign_asset,name='assign_asset'),
-path('assign_asset_user/<int:id>',views.assign_asset_user,name='assign_asset_user'),
-path('unassign_asset_user/<int:id>',views.unassign_asset_user,name='unassign_asset_user'),
-path('unassign_asset_dept/<int:id>',views.unassign_asset_dept,name='unassign_asset_dept'),
+    path('employeedetails/<int:id>', views.employeedetails,name='employeedetails'),
 
 
-    # path('employeedetails/<int:id>', views.employeedetails,name='employeedetails'),
 
-    # path('department_detail/<int:id>',views.department_detail,name='department_detail'),
-    # path('assets/',views.assets,name='assets'),
+    path('department_detail/<int:id>',views.department_detail,name='department_detail'),
+    path('assets/',views.assets,name='assets'),
+    path('dept_assets/',views.dept_assets,name='dept_assets'),
 
-    # path('update/asset/<int:id>/',views.update_asset,name='assetform'),
-    # path('employeeassetrequest/',views.employeeassetrequest,name='employeeassetrequest'),
-    # # path('employeeasset',views.employeeasset,name='employeeasset'),
-    # path('assign_asset/<int:id>',views.assign_asset,name='assign_asset'),
+    path('update/asset/<int:id>/',views.update_asset,name='assetform'),
+    path('employeeassetrequest/',views.employeeassetrequest,name='employeeassetrequest'),
 
-    # path('requests', views.requests,name='requests'),
-    # path('dept_requests',views.dept_requests,name='dept_requests'),
-    # path('employee_requests/',views.employee_requests,name='employee_requests'),
-    # path('employeerequestdetails/<int:id>', views.employeerequestdetails,name='employeerequestdetails'),
-    # path('managerrequestdetails/<int:id>', views.managerrequestdetails ,name='managerrequestdetails'),
-    # path('add_departments/',views.add_departments,name='add_departments'),
+    path('assign_asset/<int:id>',views.assign_asset,name='assign_asset'),
+    path('assign_asset_user/<int:id>',views.assign_asset_user,name='assign_asset_user'),
+    path('unassign_asset_user/<int:id>',views.unassign_asset_user,name='unassign_asset_user'),
+    path('unassign_asset_dept/<int:id>',views.unassign_asset_dept,name='unassign_asset_dept'),
 
 
+    path('employeedetails/<int:id>', views.employeedetails,name='employeedetails'),
+
+    path('department_detail/<int:id>',views.department_detail,name='department_detail'),
+    path('assets/',views.assets,name='assets'),
+
+    path('update/asset/<int:id>/',views.update_asset,name='assetform'),
+    path('employeeassetrequest/',views.employeeassetrequest,name='employeeassetrequest'),
+    # path('employeeasset',views.employeeasset,name='employeeasset'),
+    path('assign_asset/<int:id>',views.assign_asset,name='assign_asset'),
+
+    path('requests', views.requests,name='requests'),
+    path('dept_requests',views.dept_requests,name='dept_requests'),
+    path('employee_requests/',views.employee_requests,name='employee_requests'),
+    path('employeerequestdetails/<int:id>', views.employeerequestdetails,name='employeerequestdetails'),
+    path('managerrequestdetails/<int:id>', views.managerrequestdetails ,name='managerrequestdetails'),
+    path('add_departments/',views.add_departments,name='add_departments'),
 
 
 
 
 
 
-path('requests', views.requests,name='requests'),
-path('requestdetails/<int:id>', views.requestdetails,name='requestdetails'),
-path('add_departments/',views.add_departments,name='add_departments'),
+
+
+    path('requests', views.requests,name='requests'),
+
+    path('add_departments/',views.add_departments,name='add_departments'),
 ]
 
 if settings.DEBUG:
