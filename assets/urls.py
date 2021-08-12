7f167dafa16b4319c449312730f12b1c92eae5fa
jn-mic <jackotienokey@gmail.com>
2021-08-12 15:17:27 +0300

    model

--- assets/urls.py
+++ assets/urls.py
@@ -41,33 +41,27 @@ urlpatterns=[
     path('add_departments/',views.add_departments,name='add_departments'),
 
 
-
-path('employees',views.employees,name='employees'),
-path('dept_employees',views.dept_employees,name='dept_employees'),
-path('employee_assets',views.employee_assets,name='employee_assets'),
-
-
-path('dashboard',DashBoardView,name='dashboard'),
-path('manager_dashboard',views.managerDashBoardView,name='manager_dashboard'),
-path('employee_dashboard',views.employeeDashBoardView,name='employee_dashboard'),
-
-path('employeedetails/<int:id>', views.employeedetails,name='employeedetails'),
+    path('employees',views.employees,name='employees'),
+    path('dept_employees',views.dept_employees,name='dept_employees'),
+    path('employee_assets',views.employee_assets,name='employee_assets'),
 
 
+    path('manager_dashboard',views.managerDashBoardView,name='manager_dashboard'),
+    path('employee_dashboard',views.employeeDashBoardView,name='employee_dashboard'),
 
-path('department_detail/<int:id>',views.department_detail,name='department_detail'),
-path('assets/',views.assets,name='assets'),
-path('dept_assets/',views.dept_assets,name='dept_assets'),
+    path('department_detail/<int:id>',views.department_detail,name='department_detail'),
+    path('assets/',views.assets,name='assets'),
+    path('dept_assets/',views.dept_assets,name='dept_assets'),
 
-path('update/asset/<int:id>/',views.update_asset,name='assetform'),
-path('employeeassetrequest/',views.employeeassetrequest,name='employeeassetrequest'),
+    path('update/asset/<int:id>/',views.update_asset,name='assetform'),
+    path('employeeassetrequest/',views.employeeassetrequest,name='employeeassetrequest'),
 
-path('assign_asset/<int:id>',views.assign_asset,name='assign_asset'),
-path('assign_asset_user/<int:id>',views.assign_asset_user,name='assign_asset_user'),
-path('unassign_asset_user/<int:id>',views.unassign_asset_user,name='unassign_asset_user'),
+    path('assign_asset/<int:id>',views.assign_asset,name='assign_asset'),
+    path('assign_asset_user/<int:id>',views.assign_asset_user,name='assign_asset_user'),
+    path('unassign_asset_user/<int:id>',views.unassign_asset_user,name='unassign_asset_user'),
 
-path('requests', views.requests,name='requests'),
-path('requestdetails/<int:id>', views.requestdetails,name='requestdetails'),
+    path('requests', views.requests,name='requests'),
+    path('requestdetails/<int:id>', views.requestdetails,name='requestdetails'),
 
 
 
