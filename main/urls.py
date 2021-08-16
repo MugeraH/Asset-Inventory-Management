52720d4b3665d6fd38a03992e96d82ae73bf4013
jn-mic <jackotienokey@gmail.com>
2021-08-16 14:49:44 +0300

    rest api endpoint

--- main/urls.py
+++ main/urls.py
@@ -11,8 +11,28 @@ from django.contrib.auth.views import(
 from users.views import SignupView
 from assets import views as user_views
 
+from django.urls import path, include
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
 
 urlpatterns = [
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
     path('',include("assets.urls")),
     path('profile/', user_views.profile, name='profile'),
